67733b7f2ab849a49ffa4ffe66f70bcd03d88cd2
Tejas Dinkar <tejas@gja.in>
2017-12-16 03:12:50 +0530

    Assets transformed on server

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = function startDevMode(opts) {
   require("babel-register")(Object.assign({
     presets: ["react"],
-    plugins: ["transform-es2015-modules-commonjs"]
+    plugins: [
+      "transform-es2015-modules-commonjs",
+      "transform-assets-import-to-string"
+    ]
   }, opts.babelOpts));
 }
