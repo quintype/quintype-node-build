7545ac228d1e079217a8c27d1f85287897df202a
srjn45 <srajanpathak45@gmail.com>
2018-10-29 16:22:20 +0530

    css architecture change

--- webpack-config.js
+++ webpack-config.js
@@ -27,62 +27,66 @@ exports.webpackConfig = function webpackConfig(publisherName, currentDirectory, 
   const config =
     process.env.NODE_ENV == "production"
       ? {
-          outputFileName: suffix => `[name]-[hash:20].${suffix}`,
-          sassLoader: [MiniCssExtractPlugin.loader, {
-            loader: "css-loader",
-          }, {
-            loader: "sass-loader",
-          }],
-          cssModuleLoader: [MiniCssExtractPlugin.loader, {
-            loader: "css-loader", options: {modules: true, importLoaders: 1, localIdentName: "[name]__[local]__[hash:base64:5]"}
-          },{
+        outputFileName: suffix => `[name]-[hash:20].${suffix}`,
+        cssModuleLoader: [MiniCssExtractPlugin.loader, {
+          loader: "css-loader", options: { modules: true, importLoaders: 1, localIdentName: "[name]__[local]__[hash:base64:5]" },
+        }, {
+          loader: "postcss-loader",
+          options: {
+            ident: "postcss",
+            sourceMap: true,
+            plugins: (loader) => [
+              require("autoprefixer"),
+              require("precss")()
+            ]
+          }
+        }, { loader: "sass-loader" }, {
+          loader: "sass-resources-loader",
+          options: {
+            resources: "./app/assets/stylesheets/base/tools/functions.m.css"
+          }
+        }],
+        cssFile: `[name]-[contenthash:20].css`,
+        compressCSSPlugins: [new OptimizeCssAssetsPlugin({
+          cssProcessorOptions: { safe: true }
+        })],
+        outputPublicPath: PUBLIC_PATH,
+        sourceMapType: 'source-map'
+      }
+      : {
+        outputFileName: suffix => `[name].${suffix}`,
+        cssModuleLoader: [
+          { loader: "style-loader" },
+          { loader: "css-loader", options: { sourceMap: true, modules: true, importLoaders: 1, localIdentName: "[name]__[local]__[hash:base64:5]" } },
+          {
             loader: "postcss-loader",
             options: {
               ident: "postcss",
               sourceMap: true,
               plugins: (loader) => [
-                require("precss")(),
-                require("autoprefixer")
+                require("autoprefixer"),
+                require("precss")()
               ]
             }
-          }],
-          cssFile: `[name]-[contenthash:20].css`,
-          compressCSSPlugins: [new OptimizeCssAssetsPlugin({
-            cssProcessorOptions: { safe: true }
-          })],
-          outputPublicPath: PUBLIC_PATH,
-          sourceMapType: 'source-map'
-        }
-      : {
-          outputFileName: suffix => `[name].${suffix}`,
-          sassLoader: [{loader: "style-loader"}, {loader: "css-loader", options: {sourceMap: true}}, {loader: "sass-loader", options: {sourceMap: true}}],
-          cssModuleLoader: [
-            {loader: "style-loader"},
-            {loader: "css-loader", options: {sourceMap: true, modules: true, importLoaders: 1, localIdentName: "[name]__[local]__[hash:base64:5]"}},
-            {
-              loader: "postcss-loader",
-              options: {
-                ident: "postcss",
-                sourceMap: true,
-                plugins: (loader) => [
-                  require("precss")(),
-                  require("autoprefixer")
-                ]
-              }
+          }, { loader: "sass-loader" }, {
+            loader: "sass-resources-loader",
+            options: {
+              resources: "./app/assets/stylesheets/base/tools/functions.m.css"
             }
-          ],
-          cssFile: `[name].css`,
-          compressCSSPlugins: [],
-          outputPublicPath: "http://localhost:8080" + PUBLIC_PATH,
-          sourceMapType: 'eval-source-map'
-        };
+          }
+        ],
+        cssFile: `[name].css`,
+        compressCSSPlugins: [],
+        outputPublicPath: "http://localhost:8080" + PUBLIC_PATH,
+        sourceMapType: 'eval-source-map'
+      };
 
   const entryFiles = Object.assign({
     app: "./app/client/app.js",
     serviceWorkerHelper: "./app/client/serviceWorkerHelper.sjs"
   }, opts.entryFiles);
 
-  if(fs.existsSync("./app/client/polyfill.js")) {
+  if (fs.existsSync("./app/client/polyfill.js")) {
     entryFiles["polyfill"] = "./app/client/polyfill.js";
   }
 
@@ -99,7 +103,6 @@ exports.webpackConfig = function webpackConfig(publisherName, currentDirectory, 
         { test: /\.jsx?$/, exclude: /node_modules/, use: BABEL_PRESET },
         { test: /\.jsx?$/, include: /node_modules\/@quintype\/framework/, use: BABEL_PRESET },
         { test: /\.jsx?$/, include: /node_modules\/@quintype\/components\/store/, use: BABEL_PRESET },
-        { test: /\.(sass|scss)$/, use: config.sassLoader },
         { test: /\.module.css$/, use: config.cssModuleLoader },
         { test: /\.m.css$/, use: config.cssModuleLoader },
         {
@@ -114,7 +117,7 @@ exports.webpackConfig = function webpackConfig(publisherName, currentDirectory, 
     },
     plugins: [
       new webpack.EnvironmentPlugin({ NODE_ENV: "development" }),
-      new MiniCssExtractPlugin({filename: config.cssFile}),
+      new MiniCssExtractPlugin({ filename: config.cssFile }),
       new ManifestPlugin({
         map(asset) {
           return Object.assign(asset, {
