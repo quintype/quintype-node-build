90f42803a23fdbb23ec313707bea236ab4731025
Sinu John <sinujohn91@gmail.com>
2018-11-11 12:16:46 +0530

    add sync script

---
+++ bin-dev-scripts/sync-to.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+PATH_TO_APP="$1"
+
+if [ -z $PATH_TO_APP ] ; then
+  echo "please provide relative path to the app; eg  ../madrid "
+  exit 1
+fi
+
+rsync -r --exclude="node_modules" . "$PATH_TO_APP/node_modules/@quintype/build/"
