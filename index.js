adba7927ef0bbf3daca8b10c8d339da5d0a2c357
Sinu John <sinujohn91@gmail.com>
2018-11-27 15:35:48 +0530

    use css-modules-require-hook for loading styles in the server

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = function startDevMode(opts) {
+  require("css-modules-require-hook")({
+    generateScopedName: "[name]__[local]__[hash:base64:5]"
+  });
   require("babel-register")(
     Object.assign(
       {
@@ -6,14 +9,15 @@ module.exports = function startDevMode(opts) {
         plugins: [
           "transform-es2015-modules-commonjs",
           "dynamic-import-node",
-          "quintype-assets",
-          [
-            "react-css-modules",
-            {
-              removeImport: true,
-              generateScopedName: "[name]__[local]__[hash:base64:5]"
-            }
-          ],
+          "babel-plugin-transform-assets-import-to-string",
+          // "quintype-assets",
+          // [
+          //   "react-css-modules",
+          //   {
+          //     removeImport: true,
+          //     generateScopedName: "[name]__[local]__[hash:base64:5]"
+          //   }
+          // ],
           "transform-class-properties"
         ]
       },
