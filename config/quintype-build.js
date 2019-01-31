f1e04c9f01c53b1aa34179f166fd75bb38bbfd4d
Tejas Dinkar <tejas@gja.in>
2019-01-31 12:56:33 +0530

    feat: Removing peer dependencies

---
+++ config/quintype-build.js
@@ -0,0 +1,20 @@
+const produce = require("immer");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+module.exports = {
+  modifyWebpackConfig: function({ defaultConfig }) {
+    return produce(defaultConfig, function(draft) {
+      draft.node = { Buffer: false };
+      draft.entry.push({ font: "./app/client/font.js" });
+      if (process.env.ANALYZE_STATS === "true") {
+        draft.plugins.push(
+          new BundleAnalyzerPlugin({
+            generateStatsFile: true,
+            analyzerMode: "static",
+            statsFilename: "prod-stats.json"
+          })
+        );
+      }
+    });
+  }
+};
