43623560aff0e17ed4d3b1341b3eb7657dbd3059
Deokumarjnu <deo.kumar.jnu@gmail.com>
2021-05-03 20:13:56 +0530

    Added static assets in build

--- config/quintype-build.js
+++ config/quintype-build.js
@@ -1,11 +1,22 @@
 const { produce } = require("immer");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+var fs = require("fs");
+const path = require("path");
 
 module.exports = {
   modifyWebpackConfig: function({ defaultConfig }) {
     const config = produce(defaultConfig, function(draft) {
       draft.node = { Buffer: false };
       draft.entry["font"] = "./app/client/font.js";
+      const dir = "./app/assets/fonts";
+      fs.readdir(dir, (err, files) => {
+        if (err) return;
+        files.forEach(file => {
+          const fullPath = path.join(dir, file);
+          draft.entry[file] = fullPath;
+        });
+      });
+
       if (process.env.ANALYZE_STATS === "true") {
         draft.plugins.push(
           new BundleAnalyzerPlugin({
