eb88f1e687f0c470e18733218c568b474c32105e
Deokumarjnu <deo.kumar.jnu@gmail.com>
2021-05-10 20:36:05 +0530

    updated logic

--- config/quintype-build.js
+++ config/quintype-build.js
@@ -1,7 +1,7 @@
 const { produce } = require("immer");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const { resolve } = require("path");
-const { readdir } = require("fs").promises;
+const fs = require("fs");
 
 module.exports = {
   modifyWebpackConfig: function({ defaultConfig }) {
@@ -9,24 +9,22 @@ module.exports = {
       draft.node = { Buffer: false };
       draft.entry["font"] = "./app/client/font.js";
 
-      async function getFiles(dir) {
-        const dirents = await readdir(dir, { withFileTypes: true });
-        const files = await Promise.all(
-          dirents.map(dirent => {
-            const res = resolve(dir, dirent.name);
-            return dirent.isDirectory()
-              ? getFiles(res)
-              : { fileName: dirent.name, filePath: res };
-          })
-        );
-        return files.flat();
-      }
-
       const dir = "./app/static-assets";
 
-      getFiles(dir).then(files =>
-        files.forEach(file => (draft.entry[file.fileName] = file.filePath))
-      );
+      function getFiles(dir) {
+        const dirents = fs.readdirSync(dir, { withFileTypes: true });
+        const files = dirents.map(dirent => {
+          const res = resolve(dir, dirent.name);
+          return dirent.isDirectory()
+            ? getFiles(res)
+            : { fileName: dirent.name, filePath: res };
+        });
+        return files.flat();
+      }
+      const files = getFiles(dir);
+      files.forEach(file => {
+        draft.entry[file.fileName] = file.filePath;
+      });
 
       if (process.env.ANALYZE_STATS === "true") {
         draft.plugins.push(
