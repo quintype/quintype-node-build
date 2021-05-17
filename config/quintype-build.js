b83f81339f641fcca3114b563ea85438e31d9079
Deo Kumar <40450564+Deokumarjnu@users.noreply.github.com>
2021-05-17 10:55:41 +0530

    feat(file-loader): Compile static assets :zap:  (#53)

--- config/quintype-build.js
+++ config/quintype-build.js
@@ -1,11 +1,30 @@
 const { produce } = require("immer");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { resolve } = require("path");
+const fs = require("fs");
 
 module.exports = {
   modifyWebpackConfig: function({ defaultConfig }) {
     const config = produce(defaultConfig, function(draft) {
       draft.node = { Buffer: false };
       draft.entry["font"] = "./app/client/font.js";
+
+      const staticAssets = "./app/static-assets";
+      function getFiles(staticAssets) {
+        const dirents = fs.readdirSync(staticAssets, { withFileTypes: true });
+        const files = dirents.map(dirent => {
+          const res = resolve(staticAssets, dirent.name);
+          return dirent.isDirectory()
+            ? getFiles(res)
+            : { fileName: dirent.name, filePath: res };
+        });
+        return files.flat();
+      }
+      fs.existsSync(staticAssets) &&
+        getFiles(staticAssets).forEach(file => {
+          draft.entry[file.fileName] = file.filePath;
+        });
+
       if (process.env.ANALYZE_STATS === "true") {
         draft.plugins.push(
           new BundleAnalyzerPlugin({
